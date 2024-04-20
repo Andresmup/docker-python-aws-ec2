546dac945c7a18488cb555855f35a7791888f21e
Andresmup <andresmunozpampillon@gmail.com>
2024-04-19 22:07:06 -0300

    Change producer

--- scr/main.py
+++ scr/main.py
@@ -4,9 +4,22 @@ import os
 import time
 from kafka import KafkaProducer
 import argparse
+from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 
-def main(topic, bootstrap_servers,n_samples):
-    producer = KafkaProducer(security_protocol="SSL", bootstrap_servers=bootstrap_servers)
+class MSKTokenProvider():
+    def token(self):
+        token, _ = MSKAuthTokenProvider.generate_auth_token('us-east-1')
+        return token
+
+def main(topic, bootstrap_servers, n_samples):
+    tp = MSKTokenProvider()
+
+    producer = KafkaProducer(
+        bootstrap_servers=bootstrap_servers,
+        security_protocol='SASL_SSL',
+        sasl_mechanism='OAUTHBEARER',
+        sasl_oauth_token_provider=tp,
+    )
 
     df_ecommerce_sale_report = pd.read_csv("https://raw.githubusercontent.com/Andresmup/ArchivosDataScience/main/ecommerce_sale_report.csv", index_col="index", low_memory=False)
     df_ecommerce_sample = df_ecommerce_sale_report.sample(n=int(n_samples), random_state=44)
@@ -22,7 +35,9 @@ def main(topic, bootstrap_servers,n_samples):
         
         producer.send(topic, json.dumps(data).encode('utf-8'))
         time.sleep(0.1)
-    
+
+    producer.close()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Procesar datos y enviarlos a Kafka')
     parser.add_argument('--topic', type=str, help='Nombre del topic de Kafka')
@@ -32,9 +47,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     topic = args.topic or os.environ.get('KAFKA_TOPIC')
-    servers = args.bootstrap_servers or os.environ.get('KAFKA_BOOTSTRAP_SERVERS')
-    bootstrap_servers = servers.split(',')
-
+    bootstrap_servers = args.bootstrap_servers or os.environ.get('KAFKA_BOOTSTRAP_SERVERS').split(',')
     n_samples = args.n_samples or os.environ.get('N_SAMPLES')
 
     main(topic, bootstrap_servers, n_samples)
